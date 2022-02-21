933421a0c171f17b6f511dfaa8aaf526c547cdef
Abhishek Mohabe <abhishek.m28@gmail.com>
2022-02-21 13:43:46 -0700

    All Encrypt test cases pass

--- src/amohabe/asu/edu/Caesar.java
+++ src/amohabe/asu/edu/Caesar.java
@@ -2,8 +2,34 @@ package amohabe.asu.edu;
 
 public class Caesar {
 
-    public static String cipher(String message, int offset) {
-        return "a";
+    private static final char CHARA = 'a';
+    private static final char CHARZ = 'z';
+    private static final int LETTERSIZE = 26;
+
+    public static String cipher(String input, int offset) {
+        char[] ch = input.toCharArray();
+        offset %= LETTERSIZE;
+        shiftBy(ch, offset);
+        return new String(ch);
+    }
+
+    private static void shiftBy(char[] ch, int offset) {
+        for (int i = 0; i < ch.length; ++i) {
+            if (ch[i] != ' ') {
+                ch[i] = offsetChar(ch[i], offset, CHARA, CHARZ);
+            }
+        }
+    }
+
+    private static char offsetChar(char ch, int offset, char A, char Z) {
+        ch += offset;
+        if (ch < A) {
+            return (char) (ch + LETTERSIZE);
+        }
+        if (ch > Z) {
+            return (char) (ch - LETTERSIZE);
+        }
+        return ch;
     }
 
     public static void main(String[] args) {
