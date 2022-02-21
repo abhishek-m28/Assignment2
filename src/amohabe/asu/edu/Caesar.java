9badcf3bde952017e030f577a19ad776ce0fb1cd
Abhishek Mohabe <abhishek.m28@gmail.com>
2022-02-21 15:53:02 -0700

    Included the main function for command line run

--- src/amohabe/asu/edu/Caesar.java
+++ src/amohabe/asu/edu/Caesar.java
@@ -1,5 +1,7 @@
 package amohabe.asu.edu;
 
+import java.util.Scanner;
+
 public class Caesar {
 
     private static final char CHARA = 'a';
@@ -68,7 +70,64 @@ public class Caesar {
         return result;
     }
 
+    public static int menu() {
+
+        int i = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nChoose the option:");
+        System.out.println("1: Encrypt");
+        System.out.println("2: Decrypt");
+        System.out.println("3: Quit\n");
+        System.out.println("Your selection: ");
+
+        while (true) {
+            i = scan.nextInt();
+            if (i > 0 && i < 4) {
+                break;
+            }
+            else {
+                System.out.println("Invalid Selection!");
+            }
+        }
+        return i;
+    }
+
     public static void main(String[] args) {
+
+        System.out.println("\n**********************************");
+        System.out.println("Caesar Cipher in Java");
+        System.out.println("**********************************\n");
+
+        mainloop:
+
+        while (true) {
+            switch (menu()) {
+                case 1: System.out.println("\nEncrypt selected");
+                    Scanner strmessage = new Scanner(System.in);
+                    System.out.println("Enter the string to encrypt: ");
+                    String message = strmessage.nextLine();
+                    Scanner intoffset = new Scanner(System.in);
+                    System.out.println("Enter the offset value: ");
+                    int offset = intoffset.nextInt();
+                    String cipheredMessage = cipher(message, offset);
+                    System.out.println("Offset: " + offset);
+                    System.out.println("Ciphered Message: " + cipheredMessage);
+                    break;
+                case 2: System.out.println("\nDecrypt selected");
+                    Scanner strmessage2 = new Scanner(System.in);
+                    System.out.println("Enter the string to decrypt: ");
+                    String message2 = strmessage2.nextLine();
+                    Scanner intoffset2 = new Scanner(System.in);
+                    System.out.println("Enter the offset value: ");
+                    int offset2 = intoffset2.nextInt();
+                    String cipheredMessage2 = decrypt(message2, offset2);
+                    System.out.println("Offset: " + offset2);
+                    System.out.println("Decrypted Message: " + cipheredMessage2);
+                    break;
+                case 3: System.out.println("Quiting!");
+                    break mainloop;
+            };
+        }
 
     }
 
